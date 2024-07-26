50ddd49b078f453b96cbee4f5ba64b9d4a8d2b48
CristianFerreiraM <ferreira.marques.box@gmail.com>
2024-07-25 22:09:12 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,142 @@
+let valorToltal = 0
+document.getElementById('valorTotal').innerHTML = "R$0";
+let classCorTabela = 0;
+
+//funções auxliares
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
+}
+
+
+//função para cadastro
+function somaValorDefineClass(){
+    let valorTotal = parseInt(document.getElementById('valorTotal').textContent.split('R$')[1])
+    let calabresaMussarela = parseInt(document.getElementById('valorCalabresaMussarela').textContent.split('R$')[1])
+    let quatroQueijos = parseInt(document.getElementById('valorQuatroQueijos').textContent.split('R$')[1])
+    let frangoCatupiry = parseInt(document.getElementById('valorFrangoCatupiry').textContent.split('R$')[1])
+
+  let sabor = document.getElementById("inputSabor")
+    let sabores = sabor.options[sabor.selectedIndex]
+
+    switch(sabores.value){
+        case "1":
+            valorTotal = valorTotal + calabresaMussarela
+            classCorTabela = "calabresaMussarela"
+            exibirTextoNaTela('#valorTotal', `R$${valorTotal}`)
+            break;
+        case "2":
+            valorTotal = valorTotal + quatroQueijos;
+            classCorTabela = "quatroQueijos"
+            exibirTextoNaTela('#valorTotal', `R$${valorTotal}`)
+         
+            break;
+        case "3":
+            valorTotal = valorTotal + frangoCatupiry;
+            classCorTabela = "frangoCatupiry"
+            exibirTextoNaTela('#valorTotal', `R$${valorTotal}`)
+
+            break;
+    }
+}
+
+function criaTr(itemUm, itemDois, itemTres){
+    let tabela = document.getElementById('tabelaPedido')
+
+    let tr = document.createElement("tr")
+    tr.classList.add("linha")
+    tabela.appendChild(tr)
+
+    tr.innerHTML=`<th  class="itensNome" >${itemUm}</th><th class="itensCpf">${itemDois}</th><th class="${classCorTabela}">${itemTres}</th>`
+}
+
+
+//funções validações
+function validaVazio(valida){
+    let nome = document.getElementById("inputNome")
+
+  
+    if(valida != ''){
+        nome.classList.remove('inputCadastroErro')
+        nome.classList.add('inputCadastro')
+
+        return true
+    }else{
+        nome.classList.remove('inputCadastro')
+        nome.classList.add('inputCadastroErro')
+
+     
+    }
+}
+
+function validaCpf(validaCpf){
+    let cpf = document.getElementById("inputCPF")
+    if(validaCpf.length == 11){
+        cpf.classList.remove('inputCadastroErro')
+        cpf.classList.add('inputCadastro')
+        return true
+    }else{
+        cpf.classList.remove('inputCadastro')
+        cpf.classList.add('inputCadastroErro')
+        
+    }
+}
+
+function validaSabor(validaSabor){
+    let sabor = document.getElementById("inputSabor")
+    if (validaSabor != 0){
+        sabor.classList.remove('inputCadastroErro')
+        sabor.classList.add('inputCadastro')
+        return true
+    }else{
+        sabor.classList.remove('inputCadastro')
+        sabor.classList.add('inputCadastroErro')
+        
+    }
+}
+
+//função cadastro
+function botaoCadastro(){
+    event.preventDefault()
+    let form = document.getElementById("areaCadastro")
+    let nome = document.getElementById("inputNome").value
+    let cpf = document.getElementById("inputCPF").value
+    let sabor = document.getElementById("inputSabor")
+    let sabores = sabor.options[sabor.selectedIndex]
+
+    /* validaVazio(nome)
+    validaCpf(cpf)
+    validaSabor(sabores.value) */
+
+   
+
+/*     const nomeValido = validaVazio(nome);
+    const cpfValido = validaCpf(cpf);
+    const saboresValidos = validaSabor(sabores.value); */
+    
+    if (validaVazio(nome) && validaCpf(cpf) && validaSabor(sabores.value)) {         
+
+        somaValorDefineClass()
+        criaTr(nome, cpf, sabores.text)
+
+     
+        exibirTextoNaTela('.msgErro', '')
+        exibirTextoNaTela('#msgConcluindo', 'Pedido realizado com sucesso')
+        form.reset()
+
+    } else {
+        const erroMsg = validaVazio(nome) ? (validaCpf(cpf) ? "Escolha sua pizza" : "CPF inválido") : "Nome inválido";
+        let classe = document.querySelector('.msgErro')
+        console.log(classe)
+        
+        if(classe != erroMsg){
+            let elemento = document.createElement("p")
+            elemento.classList.add('msgErro');
+            form.appendChild(elemento)
+            elemento.innerHTML = erroMsg
+             exibirTextoNaTela("#msgConcluindo", "");
+        }
+    }
+
+}
+
